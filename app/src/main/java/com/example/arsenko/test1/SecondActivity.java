227b6d006887e6272bf90fc911db97ed4bbfd8b0
Arsenko <arsen4yk@gmail.com>
2017-08-14 16:18:51 +0300

    coo

--- app/src/main/java/com/example/arsenko/test1/SecondActivity.java
+++ app/src/main/java/com/example/arsenko/test1/SecondActivity.java
@@ -3,8 +3,11 @@ package com.example.arsenko.test1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +15,10 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
         String text = intent.getStringExtra("text");
+        tv2= (TextView) findViewById(R.id.text);
+        tv2.setText(text);
 
     }
+
+
 }
